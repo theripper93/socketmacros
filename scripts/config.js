e0c69fac6628368d85802feb72529a6bb43d0658
theripper93 <tsplab@gmail.com>
2021-10-16 15:08:11 +0200

    a

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,34 @@
 Hooks.once('init', async function() {
 
+    libWrapper.register(
+        "socketmacros",
+        "Macro.prototype.execute",
+        SocketMacros.executeWrapper,
+        "MIXED"
+      );
+
 });
 
 Hooks.once('ready', async function() {
 
+});
+
+let SocketMacrosSocket;
+
+Hooks.once("socketlib.ready", () => {
+    SocketMacrosSocket = socketlib.registerModule("socketmacros");
+    SocketMacrosSocket.register("execute", SocketMacros.execute);
+});
+
+Hooks.on("renderMacroConfig", (app,html)=>{
+
+    const isSocket = app.object.getFlag("socketmacros", "isSocket");
+    const checkbox = `
+    <div class="form-group">
+            <label>Execute on all Clients?</label>
+            <input type="checkbox" name="flags.socketmacros.isSocket" data-dtype="Boolean" ${isSocket ? "checked" : ""}>
+        </div>
+    `
+
+    html.find(`select[name="type"]`).closest(".form-group").after(checkbox);
 });
---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+class SocketMacros{
+    static async execute(...args){
+        const data = args[args.length-1];
+        const macro = game.macros.get(data.id);
+        if(!macro) return;
+        macro.execute(...args);
+    }
+
+    static executeWrapper(wrapped, ...args){
+        if(this.data.flags?.socketmacros.isSocket && !args[args.length-1]?.fromSocket){
+            SocketMacrosSocket.executeForOthers("execute",...args, {id: this.id, "fromSocket":true});
+        }
+        return wrapped(...args);
+    }
+}
